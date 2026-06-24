9799fa7e6d85ba32312c2cee2a661de4b5d56dd0
Matías Alarcón <matias.alarconn.go@gmail.com>
2026-06-24 12:15:33 -0400

    feat: Añadiendo los DTOs para exponer correctamente la información desde la capa de datos

---
+++ src/main/java/com/tiendaonline/dto/ProductoRequestDTO.java
@@ -0,0 +1,50 @@
+package com.tiendaonline.dto;
+
+public class ProductoRequestDTO {
+
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+    private Integer stock;
+    private String categoria;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
